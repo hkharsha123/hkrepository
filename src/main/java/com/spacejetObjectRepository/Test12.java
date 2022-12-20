34f38a63c08d9a05f9deeb5fa6f835314350d613
hkharsha123 <hkharsha.97@gmail.com>
2022-12-20 15:44:16 +0530

    euheiof

---
+++ src/main/java/com/spacejetObjectRepository/Test12.java
@@ -0,0 +1,38 @@
+package com.spacejetObjectRepository;
+
+import org.testng.annotations.Test;
+
+public class Test12 {
+
+
+	
+	@Test
+	public void test()
+	{
+		
+		System.out.println("test1");
+	}
+	
+
+	@Test
+	public void test1()
+	{
+		
+		System.out.println("test3");
+	}
+
+	@Test
+	public void test2()
+	{
+		
+		System.out.println("test2");
+	}
+
+	@Test
+	public void test3()
+	{
+		
+		System.out.println("test3");
+	}
+	
+}
